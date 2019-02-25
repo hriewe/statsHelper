0b8b805d56425bcfff6c435e9db2d1387391008c
hriewe <hriewe12@gmail.com>
2019-02-25 18:51:25 -0500

    Added support for poisson distro

--- orderList.py
+++ orderList.py
@@ -5,6 +5,8 @@
 
 #import needed modules
 import sys
+import inquirer
+import os
 
 #Bubble Sort function to sort the data
 def bubbleSort(unsorted):
@@ -15,82 +17,108 @@ def bubbleSort(unsorted):
             unsorted[i] = unsorted[i+1]
             unsorted[i+1] = temp
 
-#Get list of numbers from user
-numString = input("Please enter the list, delimited by your cli argument: ")
-numString = numString.strip("[]()\{\}")
-array = []
+home = [inquirer.List('home', message="Please select a function: ", choices=["List Evaluator", "Poisson Distrobution", "Binomial Distrobution"],),]
+homeSelection = inquirer.prompt(home)
 
-#Split the list at the comma and add it to a python list
-if sys.argv[1] == ",":
-  for num in numString.split(','):
-    array.append(float(num))
-elif sys.argv[1] == " ":
-  for num in numString.split(" "):
-    array.append(float(num))
-elif sys.argv[1] == ":":
-  for num in numString.split(":"):
-    array.append(float(num))
-else:
-  print("Could not split your string, please enter a list seperated by: ',' ':' or a space.")
-  sys.exit()
+if homeSelection['home'] == "List Evaluator":
+  os.system('clear')
 
-#Call the bubbleSort function to sort the list
-bubbleSort(array)
+  #Get list of numbers from user
+  numString = input("Please enter the list: ")
+  numString = numString.strip("[]()\{\}")
+  array = []
 
-#Print the sorted list to the user on a single line
-print("\nSorted List: ", end=' ')
-for i in range (len(array)):
-  print(array[i], end=' ')
+  #Split the list at the comma and add it to a python list
+  if "," in numString:
+    for num in numString.split(','):
+      array.append(float(num))
+  elif " " in numString:
+    for num in numString.split(" "):
+      array.append(float(num))
+  elif ":" in numString:
+    for num in numString.split(":"):
+      array.append(float(num))
+  else:
+    print("Could not split your string, please enter a list seperated by: ',' ':' or a space.")
+    sys.exit()
 
-#Find the mean (average) of the list
-total = 0
-for i in range(len(array)):
-  total = total + array[i]
-mean = total/len(array)
-print("\nMean: " + str(mean))
+  #Call the bubbleSort function to sort the list
+  bubbleSort(array)
 
-#Find the median of the list
-if len(array) % 2 == 1:
-  print("Median: " + str(array[(len(array)-1)//2]))
-else:
-  upper = array[len(array) // 2]
-  lower = array[(len(array) // 2) - 1]
-  print("Median: " + str((upper + lower) / 2))
+  #Print the sorted list to the user on a single line
+  print("\nSorted List: ", end=' ')
+  for i in range (len(array)):
+    print(array[i], end=' ')
 
-#Find the mode of the list
-#I'm sure there is a better way to do this
-mode = 0
-modeOccur = 0
-modeS = ''
-modeDict = dict()
-valuesGreaterThanOne = 0
-for i in range(len(array)):
-  modeDict[array[i]] = array.count(array[i])
+  #Find the mean (average) of the list
+  total = 0
+  for i in range(len(array)):
+    total = total + array[i]
+  mean = total/len(array)
+  print("\nMean: " + str(mean))
 
-for key, value in modeDict.items():
-  if value > 1:
-    valuesGreaterThanOne += 1
-  if value > mode:
-    mode = key
-    modeOccur = value
+  #Find the median of the list
+  if len(array) % 2 == 1:
+    print("Median: " + str(array[(len(array)-1)//2]))
+  else:
+    upper = array[len(array) // 2]
+    lower = array[(len(array) // 2) - 1]
+    print("Median: " + str((upper + lower) / 2))
 
-if valuesGreaterThanOne == 0:
-  print("There is no mode")
-else:
+  #Find the mode of the list
+  #I'm sure there is a better way to do this
+  mode = 0
+  modeOccur = 0
+  modeS = ''
+  modeDict = dict()
+  valuesGreaterThanOne = 0
+  for i in range(len(array)):
+    modeDict[array[i]] = array.count(array[i])
+
   for key, value in modeDict.items():
-    if value == modeOccur:
-      modeS = modeS + str(key) + '  '
-  print("Mode(s): " + modeS)
+    if value > 1:
+      valuesGreaterThanOne += 1
+    if value > mode:
+      mode = key
+      modeOccur = value
 
-#Find the variance of the list
-varList = []
-varSum = 0
-for i in range(len(array)):
-  varList.append(array[i] - mean)
-  varList[i] = varList[i] * varList[i]
-  varSum = varSum + varList[i]
-  variance = varSum / (len(array) - 1)
-print("Variance: " + str(variance))
+  if valuesGreaterThanOne == 0:
+    print("There is no mode")
+  else:
+    for key, value in modeDict.items():
+      if value == modeOccur:
+        modeS = modeS + str(key) + '  '
+    print("Mode(s): " + modeS)
 
-#Find the standard deviation
-print("Standard Deviation: " + str(variance ** 0.5))
+  #Find the variance of the list
+  varList = []
+  varSum = 0
+  for i in range(len(array)):
+    varList.append(array[i] - mean)
+    varList[i] = varList[i] * varList[i]
+    varSum = varSum + varList[i]
+    variance = varSum / (len(array) - 1)
+  print("Variance: " + str(variance))
+
+  #Find the standard deviation
+  print("Standard Deviation: " + str(variance ** 0.5))
+
+if homeSelection['home'] == "Poisson Distrobution":
+  os.system('clear')
+  lam = float(input("Lambda: "))
+  x = float(input("x: "))
+  e = 2.71828
+
+  temp = lam ** x
+  temp2 = e ** -lam
+  temp3 = temp*temp2
+  num = 1
+  while x >= 1:
+    num = num * x
+    x = x - 1
+  poisson = temp3/num
+  print("Poisson: " + str(poisson))
+
+if homeSelection['home'] == "Binomial Distrobution":
+  print("work in progress")
+  sys.exit()
